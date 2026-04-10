d649fcf4f04a25dd22a1e8fef4f8a401e385123d
Arseniy Chernykh <chernykharseniy18@mail.ru>
2026-04-10 20:26:41 +0300

    feat: created http helpers

--- internal/httpcommon/bind.go
+++ internal/httpcommon/bind.go
@@ -1,1 +1,40 @@
 package httpcommon
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+var ErrInvalidJSON = errors.New("invalid json body")
+
+func DecodeJSON(r *http.Request, dst any) error {
+	defer func() {
+		_ = r.Body.Close()
+	}()
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	if err := decoder.Decode(dst); err != nil {
+		return ErrInvalidJSON
+	}
+
+	if err := decoder.Decode(&struct{}{}); err != nil && !errors.Is(err, io.EOF) {
+		return ErrInvalidJSON
+	}
+
+	return nil
+}
+
+func WriteJSON(w http.ResponseWriter, status int, payload any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	if payload == nil {
+		return
+	}
+
+	_ = json.NewEncoder(w).Encode(payload)
+}
