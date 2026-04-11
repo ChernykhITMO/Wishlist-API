4b8301474b0f87caec6472b064a8f2cb207c517f
Arseniy Chernykh <chernykharseniy18@mail.ru>
2026-04-11 19:59:07 +0300

    fix: main is clear

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,27 @@
-package app
+package main
+
+import (
+	"context"
+	"github.com/ChernykhITMO/Wishlist-API/internal/app"
+	"github.com/ChernykhITMO/Wishlist-API/internal/config"
+	"github.com/joho/godotenv"
+	"log"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	_ = godotenv.Load()
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := app.Run(ctx, cfg); err != nil {
+		log.Fatal(err)
+	}
+}
